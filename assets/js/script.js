85dc1256550903399e02f32be92d41efdec99458
Antonio Rodriguez Rodriguez <rodriguezdomingoantonio@gmail.com>
2025-12-03 00:53:07 -0300

    Prueba Módulo 4 - Conversor APIs, avance al 35%

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
 
+async function getMonedas() {
+    try{
+        const indicador = await fetch("https://mindicador.cl/api");
+        const  moneda = await indicador.json();
+        console.log(moneda);
+    } catch (e){
+        alert(e.message);
+    }
+}
+
+
+
 function fnChkGrafico() {
 
     alert("en grafico...");
 
     // Ejemplo de un checkbox con id="miCheckbox"
     const miCheckbox = document.getElementById("chk");
+    const miGraph = document.getElementById("miGraph");
+
     let valorCheckbox = null;
 
     if (miCheckbox.checked) {
@@ -15,3 +29,8 @@ function fnChkGrafico() {
     }
 
 }
+
+
+
+
+getMonedas();
